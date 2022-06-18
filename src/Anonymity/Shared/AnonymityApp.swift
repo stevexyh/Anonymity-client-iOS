4df5ba0779a51d69d2eb661a21bcad932cc0f29b
Steve X <stevexyh@hotmail.com>
2022-06-18 06:06:55 +0100

    feat(global): create Xcode project

---
+++ src/Anonymity/Shared/AnonymityApp.swift
@@ -0,0 +1,26 @@
+//
+//  File Name     : AnonymityApp.swift
+//  Project Name  : Anonymity
+//  Description   : 
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/6/18 06:01.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AnonymityApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ src/Anonymity/Tests macOS/Tests_macOSLaunchTests.swift
@@ -0,0 +1,38 @@
+//
+//  File Name     : Tests_macOSLaunchTests.swift
+//  Project Name  : Anonymity
+//  Description   : 
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/6/18 06:01.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import XCTest
+
+class Tests_macOSLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
