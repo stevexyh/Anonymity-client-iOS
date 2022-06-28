0fc2f00a1e466882cced385fe26ccd6e567c541c
Steve X <stevexyh@hotmail.com>
2022-06-28 01:12:54 +0100

    feat(Views): add toolbar TabView in MessageListView

--- src/Anonymity/Shared/Views/MessageListView.swift
+++ src/Anonymity/Shared/Views/MessageListView.swift
@@ -15,64 +15,75 @@ import SwiftUI
 
 struct MessageListView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                // Status bar
-                HStack {
-                    Image(systemName: "circle")
-                        .font(.system(size: 50))
+        TabView {
+            NavigationView {
+                VStack {
+                    // Status bar
+                    HStack {
+                        Image(systemName: "circle")
+                            .font(.system(size: 50))
 
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Steve")
-                            .font(.title)
-                        HStack {
-                            Circle()
-                                .frame(width: 10, height: 10)
-                                .foregroundColor(.green)
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Steve")
+                                .font(.title)
+                            HStack {
+                                Circle()
+                                    .frame(width: 10, height: 10)
+                                    .foregroundColor(.green)
 
-                            Text("online")
-                                .font(.system(size: 12))
-                                .foregroundColor(.gray)
-                        }
-                    }
-
-                    Spacer()
-
-                    Image(systemName: "plus.circle")
-
-                }.padding()
-
-                // Message list
-                List {
-                    ForEach(0 ..< 15) { id in
-                        HStack {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 30))
-
-                            VStack(alignment: .leading) {
-                                Text("friend \(id)")
-                                Text("This is a piece of message...")
-                                    .font(.system(size: 14))
+                                Text("online")
+                                    .font(.system(size: 12))
                                     .foregroundColor(.gray)
                             }
+                        }
 
-                            Spacer()
+                        Spacer()
 
-                            VStack {
-                                Text("\(Date().formatted(date: .omitted, time: .shortened))")
-                                    .font(.system(size: 10))
-                                    .foregroundColor(.gray)
+                        Image(systemName: "plus.circle")
+
+                    }.padding()
+
+                    // Message list
+                    List {
+                        ForEach(0 ..< 15) { id in
+                            HStack {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 30))
+
+                                VStack(alignment: .leading) {
+                                    Text("friend \(id)")
+                                    Text("This is a piece of message...")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                }
 
                                 Spacer()
+
+                                VStack {
+                                    Text("\(Date().formatted(date: .omitted, time: .shortened))")
+                                        .font(.system(size: 10))
+                                        .foregroundColor(.gray)
+
+                                    Spacer()
+                                }
                             }
                         }
                     }
+                    .listStyle(.plain)
                 }
-                .listStyle(.plain)
+                .navigationBarHidden(true)
             }
-            .navigationBarHidden(true)
+            .tabItem {
+                Label("Chats", systemImage: "quote.bubble")
+            }
+
+            // Contacts View
+            HStack {
+            }
+            .tabItem {
+                Label("Contacts", systemImage: "person.text.rectangle")
+            }
         }
-        .overlay(Text("a"))
     }
 }
 
