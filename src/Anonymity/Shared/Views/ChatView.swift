5fa9bba5d19d9dbfe98cddb84c98ec1c5004682f
Steve X <stevexyh@hotmail.com>
2022-06-28 17:03:16 +0100

    feat(Views): create ChatView

---
+++ src/Anonymity/Shared/Views/ChatView.swift
@@ -0,0 +1,26 @@
+//
+//  File Name     : ChatView.swift
+//  Project Name  : Anonymity
+//  Description   : 
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/6/28 01:35.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
