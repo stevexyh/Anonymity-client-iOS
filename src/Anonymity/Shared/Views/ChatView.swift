401e4abe7d4d2ddd275d07f94906edd56707933d
Steve X <stevexyh@hotmail.com>
2022-06-28 20:03:22 +0100

    feat(Views): finished sketch of ChatView

--- src/Anonymity/Shared/Views/ChatView.swift
+++ src/Anonymity/Shared/Views/ChatView.swift
@@ -18,12 +18,58 @@ struct ChatView: View {
     let name: String
 
     var body: some View {
-        Text("Chat with \(name)")
+        VStack {
+            Text("\(name)")
+
+            let columns = [GridItem(.flexible(minimum: 10))]
+            GeometryReader { geometry in
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        HStack {
+                            Image(systemName: "lock.open.fill")
+                            Text("Chat with \(name) has not been encrypted yet")
+                        }
+                        .font(.system(size: 14))
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(.orange.opacity(0.3))
+                        .foregroundColor(.black.opacity(0.7))
+                        .cornerRadius(20)
+                        .padding()
+
+                        ForEach(0 ..< 10) { id in
+                            HStack {
+                                ZStack {
+                                    MessageBubbleView(id: id, maxWidth: geometry.size.width * 0.8)
+                                        .padding()
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: ((id % 2) != 0) ? .leading : .trailing)
+                        }
+                    }
+                }
+            }
+            .background(.gray.opacity(0.2))
+        }
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView(name: "Alice")
+    }
+}
+
+struct MessageBubbleView: View {
+    let id: Int
+    let maxWidth: CGFloat?
+
+    var body: some View {
+        Text("\(id): test sample message test sample message test sample message test sample message ------------------------------------------")
+            .padding(.horizontal)
+            .padding(.vertical, 5)
+            .background(((id % 2) != 0) ? .gray.opacity(0.3) : .green.opacity(0.6))
+            .cornerRadius(10)
+            .frame(maxWidth: maxWidth, alignment: ((id % 2) != 0) ? .leading : .trailing)
     }
 }
