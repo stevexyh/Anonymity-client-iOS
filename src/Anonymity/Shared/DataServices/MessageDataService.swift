2a966334f6b4f55fe381e85ddaafefc7d62a1c90
Steve X <stevexyh@hotmail.com>
2022-07-05 16:48:50 +0100

    feat(DataServices): create MessageDataService

---
+++ src/Anonymity/Shared/DataServices/MessageDataService.swift
@@ -0,0 +1,23 @@
+//
+//  File Name     : MessageDataService.swift
+//  Project Name  : Anonymity
+//  Description   :
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/7/5 16:45.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import Foundation
+
+class MessageDataService {
+    static let messages: [Message] = [
+//        Message(type: <#T##Message.MessageType#>, sender: <#T##User#>, receiver: <#T##User#>, content: <#T##String#>, timestamp: <#T##Date#>, isReceived: <#T##Bool#>, digest: <#T##String#>),
+//        Message(type: <#T##Message.MessageType#>, sender: <#T##User#>, receiver: <#T##User#>, content: <#T##String#>, timestamp: <#T##Date#>, isReceived: <#T##Bool#>, digest: <#T##String#>),
+//        Message(type: <#T##Message.MessageType#>, sender: <#T##User#>, receiver: <#T##User#>, content: <#T##String#>, timestamp: <#T##Date#>, isReceived: <#T##Bool#>, digest: <#T##String#>),
+//        Message(type: <#T##Message.MessageType#>, sender: <#T##User#>, receiver: <#T##User#>, content: <#T##String#>, timestamp: <#T##Date#>, isReceived: <#T##Bool#>, digest: <#T##String#>),
+    ]
+}
