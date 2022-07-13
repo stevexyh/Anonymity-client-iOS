2961e18ae6b128e9fc2db8b7b5268e26a87d0127
Steve X <stevexyh@hotmail.com>
2022-07-13 02:17:51 +0200

    feat(DataServices): add sample data for Contact Model

---
+++ src/Anonymity/Shared/DataServices/ContactDataService.swift
@@ -0,0 +1,22 @@
+//
+//  File Name     : ContactDataService.swift
+//  Project Name  : Anonymity
+//  Description   :
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/7/13 02:13.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import Foundation
+
+class ContactDataService {
+    static let contacts: [Contact] = [
+        Contact(uid: "11-aa", firstName: "Alice", lastName: "Test"),
+        Contact(uid: "12-ab", firstName: "Bob", lastName: "Test"),
+        Contact(uid: "21-ba", firstName: "Charlie", lastName: "Test"),
+    ]
+}
