555b798c47d9108cf0f3fa9b0ba45ed3e54df15d
Steve X <stevexyh@hotmail.com>
2022-07-13 02:07:49 +0200

    feat(Models): create Model of Contact

---
+++ src/Anonymity/Shared/Models/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  File Name     : Contact.swift
+//  Project Name  : Anonymity
+//  Description   :
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.4
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/7/13 01:56.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+    let uid: String?
+    let firstName: String?
+    let lastName: String?
+}
