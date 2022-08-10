7bbcfa9f376419b1d9a8388a392649a31af2a567
Steve X <stevexyh@hotmail.com>
2022-08-10 03:10:15 +0100

    feat(playground): add ECC asymmetric enc/dec/keygen

--- src/Anonymity-Playground/Cryptography.playground/Contents.swift
+++ src/Anonymity-Playground/Cryptography.playground/Contents.swift
@@ -9,7 +9,7 @@ let secretKey = SymmetricKey(size: .bits256)
 let sealedBox = try? AES.GCM.seal(plainText, using: secretKey)
 let cipherData = sealedBox?.ciphertext
 
-// (Steve X) MARK: for transmission
+// (Steve X) MARK: For transmission
 let nonce = sealedBox?.nonce
 let tag = sealedBox?.tag
 let cipherBase64Str = cipherData?.base64EncodedString()
@@ -24,3 +24,30 @@ let decodedSealBox = try! AES.GCM.SealedBox(
 
 let decryptedData = try? AES.GCM.open(decodedSealBox, using: secretKey)
 let decryptedText = String(data: decryptedData ?? Data(), encoding: .utf8)
+
+// (Steve X) MARK: - ECC Asymmetric Encryption & KeyGen
+// (Steve X) TODO: [DISSERTATION]: Compare NIST P256/P384/P521 & Curve25519
+// For User A
+let priA = Curve25519.KeyAgreement.PrivateKey()
+let priABase64Str = priA.rawRepresentation.base64EncodedString()
+let pubA = priA.publicKey
+let pubABase64Str = pubA.rawRepresentation.base64EncodedString()
+
+// For User B
+let priB = Curve25519.KeyAgreement.PrivateKey()
+let priBBase64Str = priB.rawRepresentation.base64EncodedString()
+let pubB = priB.publicKey
+let pubBBase64Str = pubB.rawRepresentation.base64EncodedString()
+
+// (Steve X) MARK: For transmission
+let pubAData = Data(base64Encoded: pubABase64Str)
+let pubADecoded = try! Curve25519.KeyAgreement.PublicKey(rawRepresentation: pubAData ?? Data())
+let pubBData = Data(base64Encoded: pubBBase64Str)
+let pubBDecoded = try! Curve25519.KeyAgreement.PublicKey(rawRepresentation: pubBData ?? Data())
+pubADecoded.rawRepresentation == pubA.rawRepresentation
+pubBDecoded.rawRepresentation == pubB.rawRepresentation
+
+// (Steve X) MARK: Key Generate
+let secKeyA = try? priA.sharedSecretFromKeyAgreement(with: pubBDecoded)
+let secKeyB = try? priB.sharedSecretFromKeyAgreement(with: pubADecoded)
+secKeyA == secKeyB
