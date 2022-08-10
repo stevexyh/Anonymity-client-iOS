e53d054c12ea0f335d34db34e45769fa1982988b
Steve X <stevexyh@hotmail.com>
2022-08-10 02:18:01 +0100

    feat(playground): add AES symmetric enc/dec

--- src/Anonimity-Playground/Cryptography.playground/Contents.swift
+++ src/Anonimity-Playground/Cryptography.playground/Contents.swift
@@ -1,4 +1,26 @@
-import UIKit
 import CryptoKit
+import UIKit
 
-var greeting = "Hello, playground"
+// (Steve X) MARK: - AES Symmetric Encryption
+// (Steve X) MARK: Encryption
+let plainText = "Hello, playground".data(using: .utf8) ?? Data()
+let digest = SHA256.hash(data: plainText)
+let secretKey = SymmetricKey(size: .bits256)
+let sealedBox = try? AES.GCM.seal(plainText, using: secretKey)
+let cipherData = sealedBox?.ciphertext
+
+// (Steve X) MARK: for transmission
+let nonce = sealedBox?.nonce
+let tag = sealedBox?.tag
+let cipherBase64Str = cipherData?.base64EncodedString()
+
+// (Steve X) MARK: Decryption
+let decodedCipher = Data(base64Encoded: cipherBase64Str ?? "")
+let decodedSealBox = try! AES.GCM.SealedBox(
+    nonce: nonce.unsafelyUnwrapped,
+    ciphertext: decodedCipher.unsafelyUnwrapped,
+    tag: tag.unsafelyUnwrapped
+)
+
+let decryptedData = try? AES.GCM.open(decodedSealBox, using: secretKey)
+let decryptedText = String(data: decryptedData ?? Data(), encoding: .utf8)
