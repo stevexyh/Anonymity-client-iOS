1b3f4b69e9de308b3426cc82dbe0cc26b496f25a
Steve X <stevexyh@hotmail.com>
2022-08-21 02:53:00 +0100

    refactor(Extensions): extract extension for UIApplication

---
+++ src/Anonymity/Shared/Extensions/UIApplication.swift
@@ -0,0 +1,30 @@
+//
+//  File Name     : UIApplication.swift
+//  Project Name  : Anonymity
+//  Description   :
+//
+//  Swift Version : Using Swift 5.0
+//  macOS Version : Developed on macOS 12.5
+//  GitHub Page   : https://github.com/Steve-Xyh
+//  -------------------------------------------------------
+//  Created by Steve X on 2022/8/21 02:51.
+//  Copyright © 2022 Steve X Software. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIApplication {
+    var keyWindow: UIWindow? {
+        // Get connected scenes
+        return UIApplication.shared.connectedScenes
+            // Keep only active scenes, onscreen and visible to the user
+            .filter { $0.activationState == .foregroundActive }
+            // Keep only the first `UIWindowScene`
+            .first(where: { $0 is UIWindowScene })
+            // Get its associated windows
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            // Finally, keep only the key window
+            .first(where: \.isKeyWindow)
+    }
+}
